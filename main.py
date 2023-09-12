f44638d64e7b2ab5e2d92eb74b90e2ccad7cc5fc
William <r2010shadow@163.com>
2023-09-12 15:23:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,16 @@ import streamlit as st
 from datetime import time, datetime
 import time
 
-st.title('欢迎访问')
-
+st.title('欢迎访问link3.cc/zijiu')
 with st.expander('关于自我关怀专栏'):
-  st.image("https://qiniu.production.link3.cc/profile_images/1694315147716")
+  st.image("https://qiniu.production.link3.cc/profile_images/1694315147716",width="400")
   st.markdown("*从* **今天起** ***自我关怀***.")
   st.markdown('''
     :red[保健意识] :orange[身心健康] :green[体育锻炼] :blue[健康饮食] :violet[生活方式]
     :gray[卫生习惯] :rainbow[保健食品].''')
   st.markdown(":tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
 
-  multi = '''告别无数个辗转反侧的夜晚，无数个心灰意冷与叹息
-  想生活步入正轨，必须先振作自己
-  '''
+  multi = '''告别无数个辗转反侧的夜晚、心灰意冷与叹息，让生活步入正轨，必须先振作自己'''
   st.markdown(multi)
 
 st.header('自我关怀宣言', divider='rainbow')
@@ -58,3 +55,7 @@ multi = '''
 所有利益攸关方都将受益于普遍理解和接受的自我保健的统一全球战略发展，目前他们使用健康生活方式、自我管理或疾病预防等替代术语。
 '''
 st.markdown(multi)
+
+st.image("https://qiniu.production.link3.cc/profile_images/1694315147716",width="400")
+st.write("欢迎访问 https://link3.cc/zijiu")
+
