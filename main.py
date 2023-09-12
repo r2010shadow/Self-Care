1cffad3d44bac4136c0e646d3b730fbf5d2c7c0b
William <r2010shadow@163.com>
2023-09-12 10:22:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from datetime import time, datetime
 import time
 
 #st.image("https://isfglobal.org/wp-content/uploads/2019/09/7-pillars-of-self-care-Chinese-768x351.jpg")
-st.image("https://qiniu.production.link3.cc/profile_images/1694315147716")
+st.image("http://sns-img-bd.xhscdn.com/spectrum/1040g0k030otfctsqk4005p5n2gj7opl0n7nvj78?imageView2/2/w/1080/format/jpg")
 
