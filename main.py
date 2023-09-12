67dd26d5cb5d28d02f0c5e2f102155a27a3f468c
William <r2010shadow@163.com>
2023-09-12 09:54:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import streamlit as st
 from datetime import time, datetime
 import time
 
-st.image("https://isfglobal.org/wp-content/uploads/2019/09/7-pillars-of-self-care-Chinese-768x351.jpg", width=200)
+st.image("https://isfglobal.org/wp-content/uploads/2019/09/7-pillars-of-self-care-Chinese-768x351.jpg")
 
 
