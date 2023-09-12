1c794fad4df6bccc100e8c6a98a705027e3dfc2e
William <r2010shadow@163.com>
2023-09-12 14:55:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ import streamlit as st
 from datetime import time, datetime
 import time
 
-#st.image("https://isfglobal.org/wp-content/uploads/2019/09/7-pillars-of-self-care-Chinese-768x351.jpg")
-st.image("http://sns-img-bd.xhscdn.com/spectrum/1040g0k030otfctsqk4005p5n2gj7opl0n7nvj78?imageView2/2/w/1080/format/jpg")
+st.title('欢迎访问')
+
+with st.expander('关于自我关怀专栏'):
+  st.image("https://qiniu.production.link3.cc/profile_images/1694315147716")
+  st.markdown("*自我关怀* 从**今天** ***开始***.")
+  st.markdown('''
+    :red[保健意识] :orange[身心健康] :green[体育锻炼] :blue[健康饮食] :violet[生活方式]
+    :gray[卫生习惯] :rainbow[保健食品].''')
+  st.markdown("Here's a bouquet &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+
+  multi = '''
+  告别无数个辗转反侧的夜晚，无数个心灰意冷与叹息
+  想生活步入正轨，必须先振作自己
+  今天起关怀自我
+  link3.cc/zijiu
+  '''
+  st.markdown(multi)
+
+
 
