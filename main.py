88fd60f36ad4c5340eac3854f2877bbc9159a637
William <r2010shadow@163.com>
2023-09-14 13:41:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ The good news is that it is possible to reduce substantially our risk of these d
 Is self-care hard to do? Some elements certainly are, such as the difficulty that smokers have in quitting tobacco use. But there is much that most of us can do to help ourselves to stay healthy and to help prevent or delay lifestyle diseases. The way forwards for most of us is to take small steps in positive directions.
 
 '''
+st.markdown(multi)
 
 
 
