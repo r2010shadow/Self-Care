ea84af417ea06611be253eb5feb40330bcb58804
William <r2010shadow@163.com>
2023-09-14 13:40:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,32 @@ with st.expander('关于专栏'):
   multi = '''自我关怀是一种终生的习惯和文化，是个人为自己和家人保持健康而采取的行动'''
   st.markdown(multi)
 
+st.header('自我关怀',divider='rainbow')
+
+multi = '''
+:tulip::cherry_blossom::tulip:自我关怀:tulip::cherry_blossom::tulip:
+大多数人都相当健康，像孩子一样好。然而，随着年龄的增长，我们面临着生活中的挑战和诱惑，比如食物、酒精和烟草，以及久坐不动的生活方式。我们面临着选择和风险。
+
+不幸的是，这些选择的共同结果——肥胖、缺乏身体活动、吸烟、酗酒和不健康饮食——是当今心脏病发作和中风、癌症、糖尿病、慢性呼吸系统疾病和其他“非传染性疾病”流行的原因。
+
+好消息是，通过调整我们的生活方式，通过自我照顾来照顾自己，大大降低我们患这些疾病的风险是可能的。
+
+自我照顾很难做到吗?有些因素当然是这样的，比如吸烟者很难戒烟。但我们大多数人都可以做很多事情来帮助自己保持健康，帮助预防或延缓生活方式引起的疾病。对我们大多数人来说，前进的道路是朝着积极的方向迈出一小步。
+
+
+:sunflower::blossom::sunflower::blossom:Self-Care:sunflower::blossom::sunflower::blossom:
+Most people are reasonably healthy and well as children. As we get older however, we are faced with the challenges and temptations of life such as food, alcohol and tobacco, and the opportunity for sedentary lifestyles. We are faced with choices, and risks.
+
+Unfortunately the common result of these choices – obesity, physical inactivity, tobacco use, alcohol abuse and unhealthy diets – are the cause of today’s epidemic of heart attacks and strokes, cancers, diabetes, chronic respiratory disease and other ‘noncommunicable diseases.’
+
+The good news is that it is possible to reduce substantially our risk of these diseases by adjusting our lifestyles, taking more care of ourselves by practicing self-care.
+
+Is self-care hard to do? Some elements certainly are, such as the difficulty that smokers have in quitting tobacco use. But there is much that most of us can do to help ourselves to stay healthy and to help prevent or delay lifestyle diseases. The way forwards for most of us is to take small steps in positive directions.
+
+'''
+
+
+
 st.header('自我关怀宣言', divider='rainbow')
 
 multi = '''
